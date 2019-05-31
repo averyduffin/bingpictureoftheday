af5dfa510170071f7ced67d76807a9bdf1aceeb2
Avery Duffin <avery.duffin@rainfoucus.com>
2019-05-30 21:28:35 -0600

    adding a bash_profile

--- bingPictures.sh
+++ bingPictures.sh
@@ -1,24 +1,31 @@
-#!/usr/bin/env bash
+home="/Users/averyduffin"
 
-PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
+PICTURE_DIR="$home/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-urls=( $(curl -s https://www.bing.com/?cc=gb | \
+uuid=$(uuidgen)
+
+urls=( $(curl -s https://www.bing.com/ | \
     grep -Eo "url:'.*?'" | \
     sed -e "s/url:'\([^']*\)'.*/https:\/\/bing.com\1/" | \
     sed -e "s/\\\//g") )
 
 for p in ${urls[@]}; do
-    filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
-    if [ ! -f $PICTURE_DIR/$filename ]; then
+    filename="1.jpg"
+    # if [ ! -f $PICTURE_DIR/$filename ]; then
         echo "Downloading: $filename ..."
         curl -Lo "$PICTURE_DIR/$filename" $p
-    else
-        echo "Skipping: $filename ..."
-    fi
+    # else
+    #     echo "Skipping: $filename ..."
+    # fi
 done
 
 ImageFilePath=$PICTURE_DIR/$filename
 
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$ImageFilePath'" && killall Dock
+killall Dock
+
+# sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$ImageFilePath'" && killall Dock;
+
+# osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/averyduffin/Pictures/bing-wallpapers/1.jpg"'
+# killall Dock
